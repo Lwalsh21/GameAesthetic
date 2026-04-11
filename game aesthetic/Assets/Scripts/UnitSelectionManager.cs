ebf1e41e8309999790049137e3e16e1c62d650c8
Lwalsh21 <lwalsh21@iu.edu>
2026-04-11 16:14:05 -0400

    update

---
+++ game aesthetic/Assets/Scripts/UnitSelectionManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelectionManager : MonoBehaviour
+{
+  public static UnitSelectionManager Instance { get; set; }
+
+    public List<GameObject> allUnitsList = new List<GameObject>();
+    public List<GameObject> unitsSelected = new List<GameObject>();
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(GameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+}
---
+++ game aesthetic/Assets/Scripts/unit.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unit : MonoBehaviour
+{
+    void Start()
+    {
+        UnitSelectionManager.Instance.allUnitsList.Add(gameObject)
+    }
+
+    private void OnDestroy()
+    {
+        UnitSelectionManager.Instance.allUnitsList.Remove(gameObject);
+    }
+}
